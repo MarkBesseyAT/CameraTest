d829bcbd5cccfa3fb6a1feced2f4d88f3cc83ad8
MarkBesseyAT <92050936+MarkBesseyAT@users.noreply.github.com>
2022-03-03 12:36:02 -0800

    Initial Commit

---
+++ CameraTest/CameraTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraTestApp.swift
+//  CameraTest
+//
+//  Created by Mark Bessey on 3/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CameraTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CameraTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CameraTest
+//
+//  Created by Mark Bessey on 3/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
